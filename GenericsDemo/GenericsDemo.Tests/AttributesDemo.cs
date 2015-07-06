c3fd4b32baddc2a890663b7e113e69837186f8d3
Manuel Riezebosch <mriezebosch@gmail.com>
2015-07-06 15:49:04 +0200

    attribute demo

---
+++ GenericsDemo/GenericsDemo.Tests/AttributesDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+using System.Xml;
+using System.Diagnostics;
+
+namespace GenericsDemo.Tests
+{
+    
+    class CustomAttribute : Attribute
+    {
+        public int Waarde { get; set; }
+    }
+
+    class PersoonAttribute
+    {
+        [Custom(Waarde = 15)]
+        public string Naam { get; set; }
+    }
+
+    [TestClass]
+    public class AttributesDemo
+    {
+        [TestMethod]
+        [ReadOnly(true)]
+        public void TestMethod1()
+        {
+            var type = this.GetType();
+            foreach (var method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+                foreach (var attr in method.GetCustomAttributes(true))
+                {
+                    Console.WriteLine("  [{0}]", attr.GetType().Name);
+                    TestXmlAttributes();
+                    Debug.WriteLine("asdf");
+                }
+            }
+        }
+
+        [Conditional("asdfasdfasdf")]
+        [TestMethod]
+        public void TestXmlAttributes()
+        {
+#if PGGM
+            XmlWriter writer = XmlWriter.Create("output.xml");
+#endif
+        }
+    }
+}
