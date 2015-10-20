e434e3fd20aabae69dc66a9db5f750473ca299c4
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-20 10:40:34 +0200

    ontbrekende voorbeelden teruggezet.

--- DelegateDemo/DelegateDemo/UnitTest1.cs
+++ DelegateDemo/DelegateDemo/UnitTest1.cs
@@ -36,6 +36,37 @@ namespace DelegateDemo
             // Misschien is het zo duidelijker wat er gebeurd:
             var namen2 = lijstje.Where(BevatDitItemEenU);
             PrintNamen(namen2);
+
+            // Ouderwetse tussenvorm: anonymous method die inline gedefinieerd wordt:
+            var namen3 = lijstje.Where(delegate (string n) { return n.Contains("u"); });
+
+            // Verschillende schrijfwijzen van een lambda.
+            
+            // Volledig:
+            var namen4 = lijstje.Where((string n) => { return n.Contains("u"); });
+
+            // Als het type van "n" automatisch kan worden afgeleid mag je die weglaten:
+            var namen5 = lijstje.Where(n => { return n.Contains("u"); });
+
+            // Als je "body" maar 1 statement bevat mag je de accolade's en het return keyword ook weglaten
+            var namen6 = lijstje.Where(n => n.Contains("u"));
+
+            // Je kunt 'm ook opschrijven met comprehension of query syntax:
+            var namen7 = from n in lijstje
+                         where n.Contains("u")
+                         select n;
+
+            // Dit is vooral nuttig als je wat uitgebreidere query hebt:
+            var namen8 = lijstje
+                .Where(n => n.Contains("u"))
+                .OrderBy(n => n.Length)
+                .ThenBy(n => n)
+                .Select(n => n[0]);
+
+            var namen9 = from n in lijstje
+                         where n.Contains("u")
+                         orderby n.Length, n
+                         select n[0];
         }
 
         private bool BevatDitItemEenU(string arg)
