edd31ed0c8f4bf4887a23646d9e13d66a5a6b1e3
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-23 13:53:53 +0200

    Attribute demo

---
+++ AttributeDemo/AttributeDemo/MijnEigenAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AttributeDemo
+{
+    internal class MijnEigenAttribute : Attribute
+    {
+        public int Value
+        {
+            get; private set;
+        }
+
+
+        public MijnEigenAttribute(int value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ AttributeDemo/AttributeDemo/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace AttributeDemo
+{
+   
+    [MijnEigen(6)]
+    class MyClass
+    {
+        
+        public int MyProperty { get; set; }
+    }
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PrintAllAttributes(typeof(MyClass));
+            Debug.Write("Hoi, blijkbaar in debug mode gecompileerd");
+        }
+
+        [Conditional("DEBUG")]
+        private void PrintAllAttributes(Type type)
+        {
+            foreach (var attribute in type.GetCustomAttributes(true))
+            {
+                Console.WriteLine(attribute.GetType().Name);
+                if (attribute is MijnEigenAttribute)
+                {
+                    Console.WriteLine($"Value: {((MijnEigenAttribute)attribute).Value}");
+                }
+            }
+        }
+    }
+}
