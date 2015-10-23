ef31e6f45e996187b2c6fb4bdc61289b7fe13458
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-23 12:27:18 +0200

    delegate demo

--- DelegateDemo/DelegateDemo/UnitTest1.cs
+++ DelegateDemo/DelegateDemo/UnitTest1.cs
@@ -32,13 +32,73 @@ namespace DelegateDemo
             new Persoon { Naam = "Marlies", Woonplaats = "Nijmegen" },
             new Persoon { Naam = "Tim", Woonplaats = "Veendaal" },
             new Persoon { Naam = "Fred", Woonplaats = "Driebergen" },
-
         };
 
         [TestMethod]
-        public void TestMethod1()
+        public void DitIsEenVoorbeeldVanEenLambda()
         {
+            foreach (var persoon in items.Where(p => p.Woonplaats == "Zeist"))
+            {
+                Console.WriteLine(persoon.Naam);
+            }
+        }
 
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            foreach (var persoon in WhereWoonplaastIsZeist1(items))
+            {
+                Console.WriteLine(persoon.Naam);
+            }
+        }
+
+        delegate bool SelecteerMethode(Persoon p);
+
+        [TestMethod]
+        public void MyTestMethod2()
+        {
+            foreach (var persoon in Where(items, WhereWoonplaatsIsZeist2))
+            {
+                Console.WriteLine(persoon.Naam);
+            }
+        }
+
+        private static IEnumerable<Persoon> WhereWoonplaastIsZeist1(IEnumerable<Persoon> lijstje)
+        {
+            foreach (var persoon in lijstje)
+            {
+                if (persoon.Woonplaats == "Zeist")
+                {
+                    yield return persoon;
+                }
+            }
+        }
+
+        private static IEnumerable<Persoon> Where(IEnumerable<Persoon> lijstje, SelecteerMethode methode)
+        {
+            foreach (var persoon in lijstje)
+            {
+                if (methode(persoon))
+                {
+                    yield return persoon;
+                }
+            }
+        }
+
+        private static bool WhereWoonplaatsIsZeist2(Persoon p)
+        {
+            return p.Woonplaats == "Zeist";
+        }
+
+        [TestMethod]
+        public void VerschillendeNotaties()
+        {
+            var result1 = Where(items, WhereWoonplaatsIsZeist2);
+            var result2 = Where(items, delegate(Persoon p) { return p.Woonplaats == "Zeist"; });
+
+            var result3 = Where(items, (Persoon p) => { return p.Woonplaats == "Zeist"; });
+            var result4 = Where(items, (Persoon p) => p.Woonplaats == "Zeist");
+            var result5 = Where(items, p => p.Woonplaats == "Zeist");
         }
     }
 }
