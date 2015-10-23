3e86ebad00466b32c8a2419638b80a61a1190b90
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-23 10:45:38 +0200

    SortedList met Generics

---
+++ GenericsDemo/GenericsDemo.Tests/SortedListTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace GenericsDemo.Tests
+{
+    [TestClass]
+    public class SortedListTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SortedList<int> list = new SortedList<int>();
+            list.Add(5);
+            Assert.AreEqual(5, list[0]);
+        }
+
+        [TestMethod]
+        public void Add3Before5()
+        {
+            SortedList<int> list = new SortedList<int>();
+            list.Add(5);
+            list.Add(3);
+            Assert.AreEqual(3, list[0]);
+            Assert.AreEqual(5, list[1]);
+        }
+
+        [TestMethod]
+        public void Add5After3()
+        {
+            SortedList<int> list = new SortedList<int>();
+            list.Add(3);
+            list.Add(5);
+            Assert.AreEqual(3, list[0]);
+            Assert.AreEqual(5, list[1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IndexOnEmptyListShouldThrow()
+        {
+            SortedList<int> list = new SortedList<int>();
+            var item = list[0];
+        }
+        [TestMethod]
+        public void SortedListShouldBeEnumerable()
+        {
+            SortedList<int> list = new SortedList<int> { 4, 2, 5, 2, 3, 5 };
+            var sb = new StringBuilder();
+
+            foreach (var item in list)
+            {
+                sb.Append(item);
+            }
+
+            Assert.AreEqual("223455", sb.ToString());
+        }
+    }
+
+
+}
---
+++ GenericsDemo/GenericsDemo/SortedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericsDemo
+{
+    public class SortedList<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        LinkedList<T> items = new LinkedList<T>();
+        public void Add(T item)
+        {
+            // search in "items" for correct position
+            // and add new item there
+            var node = items.First;
+            while (node != null && node.Value.CompareTo(item) < 0)
+            {
+                node = node.Next;
+            }
+
+            if (node == null)
+            {
+                items.AddLast(new LinkedListNode<T>(item));
+            }
+            else
+            {
+                items.AddBefore(node, new LinkedListNode<T>(item));
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                var node = items.First;
+                while (node != null && index-- > 0)
+                {
+                    node = node.Next;
+                }
+
+                if (node == null)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return node.Value;
+            }
+        }
+    }
+}
