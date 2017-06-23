31e01031359fc0faaa0dfba6fc5b5835110908f4
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-23 14:06:14 +0200

    attributes

---
+++ XUnitTestProject1/AttributesDemo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class AttributesDemo
+    {
+        [Fact]
+        public void WatZijnAttributes()
+        {
+            var sut = new EigenClass();
+
+            var attr = sut
+                .GetType()
+                .GetTypeInfo()
+                .GetCustomAttribute<MijnEigenAttribute>();
+            
+            Assert.NotNull(attr);
+            Assert.Equal("wortel", attr.Data);
+            Assert.Equal(10, attr.Verplicht);
+        }
+
+        [Fact]
+        public void ConditionalCompilation()
+        {
+            Print("hallo");
+        }
+
+        [Conditional("DEBUG")]
+        private void Print(string input)
+        {
+#if DEBUG
+            Console.WriteLine(input);
+#endif
+            Debug.Write(input);
+        }
+    }
+
+    internal class MijnEigenAttribute : Attribute
+    {
+        public MijnEigenAttribute(int verplicht)
+        {
+            this.Verplicht = verplicht;
+        }
+
+        public int Verplicht { get; private set; }
+        public string Data { get; set; }
+    }
+
+    [MijnEigen(10, Data = "wortel")]
+    internal class EigenClass
+    {
+    }
+
+}
